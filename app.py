a27b317ef72661f1da684d4d673103acbab9ec66
Louis <edlin1102@gmail.com>
2022-01-14 16:23:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == "image":
-        message = ImageSendMessage(original_content_url='https://storage.cloud.google.com/grounded-plane-332001.appspot.com/temp/1.jpg',    preview_image_url='https://storage.cloud.google.com/grounded-plane-332001.appspot.com/temp/1.jpg')
+        message = ImageSendMessage(original_content_url='https://storage.googleapis.com/catlife/catlife/image.jpg',    preview_image_url='https://storage.googleapis.com/catlife/catlife/image.jpg')
     else:  
         message = TextSendMessage(text=event.message.text)
 
